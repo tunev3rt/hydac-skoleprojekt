396ac4b32e7bcbeb6bdae0dce85cab3c8748a211
tunev3rt <seb2411@hotmail.com>
2024-09-24 16:52:03 +0200

    Færdigt projekt

---
+++ MenuSystem/Medarbejderlogin.cs
@@ -0,0 +1,27 @@
+﻿namespace Hydac;
+
+	public class Medarbejderlogin
+	{
+		public string login(string adgangskode) // Tager en adgangskode som argument og returnere navnet tilsvarende til adgangskoden
+		{
+		string medarbejdernavn = " ";
+		if (adgangskode == "123")
+		{
+			medarbejdernavn = "John Johnson";
+			return medarbejdernavn;
+		}
+		else if (adgangskode == "321")
+		{
+			medarbejdernavn = "Peter Petersen";
+			return medarbejdernavn;
+		}
+		else if (adgangskode == "222")
+		{
+			medarbejdernavn = "Lars Larsen";
+			return medarbejdernavn;
+		}
+		else
+			return medarbejdernavn;
+		}
+	}
+
